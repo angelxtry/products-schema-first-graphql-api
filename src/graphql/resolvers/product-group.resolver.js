ae54cf11913ef21dd6548cfd7c69a61778dd5566
sean <sean@gomicorp.com>
2022-03-28 18:53:47 +0900

    productGroup 관련 코드 추가

--- src/graphql/resolvers/product-group.resolver.js
+++ src/graphql/resolvers/product-group.resolver.js
@@ -1,7 +1,20 @@
 export const productGroupResolver = {
   Query: {
-    productGroups: (_, __, { productGroups }) => {
-      return productGroups;
+    productGroups: (_, { filter }, { productGroups }) => {
+      let result = productGroups;
+      if (filter?.productGroupName) {
+        result = result.filter((pg) =>
+          pg.productGroupName.includes(filter.productGroupName),
+        );
+      }
+      if (filter?.companyName) {
+        result = result.filter((pg) => pg.companyName.includes(filter.companyName));
+      }
+      return result;
+    },
+
+    productGroup: (_, { productGroupId }, { productGroups }) => {
+      return productGroups.find((productGroup) => productGroup.id === productGroupId);
     },
   },
 };
